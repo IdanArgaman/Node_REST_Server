3dbba9cf7e823b4602a47ece30e28649a5646e52
Idan <IdanArgaman@gmail.com>
2021-05-01 10:54:33 +0300

    Add dotenv

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import { ServerConfig } from "./config";
 import { routerV1, routerV2, redirectRouter } from "./routes";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 function main() {
     const PORT = process.env.PORT || 3000;
     const server = new ServerConfig({
