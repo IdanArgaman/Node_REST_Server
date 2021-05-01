1dcf0b03ce31c8d9055ac0a76c91218418790c80
Idan <IdanArgaman@gmail.com>
2021-05-01 11:09:38 +0300

    Add config keys

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import { ServerConfig } from "./config";
 import { routerV1, routerV2, redirectRouter } from "./routes";
+import { ConfigService } from "./services";
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 function main() {
-    const PORT = process.env.PORT || 3000;
+    const PORT = ConfigService.get(ConfigService.KEYS.PORT) || 3000;
+
     const server = new ServerConfig({
         port: PORT,
         // middleware: [],
--- services/config.service.js
+++ services/config.service.js
@@ -1,5 +1,9 @@
 export default class ConfigService {
   static NODE_ENV = process.env.NODE_ENV;
+  
+  static KEYS = {
+    PORT: 'PORT'
+  }
 
   static get(name) {
     return process.env[name];
